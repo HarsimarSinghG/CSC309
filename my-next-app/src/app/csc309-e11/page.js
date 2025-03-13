40d35c1d19a17f51f834787e88bfd762bc64c4cf
Harsimar Singh <harsimar985@gmail.com>
2025-03-13 17:59:12 -0400

    Done

--- my-next-app/src/app/csc309-e11/page.js
+++ my-next-app/src/app/csc309-e11/page.js
@@ -1,11 +1,9 @@
 // src/app/csc309-e11/page.js
 
 export default function Csc309E11Page() {
-  const utorid = "harsima1"; // Replace this with your actual UTorID
-
   return (
     <div id="auto-tester">
-      Hi. I am {utorid}, and I have learned how to develop a full-stack web app
+      Hi. I am harsima1, and I have learned how to develop a full-stack web app
       and deploy it!
     </div>
   );
