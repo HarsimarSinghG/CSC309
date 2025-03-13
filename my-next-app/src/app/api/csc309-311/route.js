d0e0cb3edc45fe7c9223db05a655f51e82e3d074
Harsimar Singh <harsimar985@gmail.com>
2025-03-13 17:35:27 -0400

    Done

---
+++ my-next-app/src/app/api/csc309-311/route.js
@@ -0,0 +1,15 @@
+export async function GET(req) {
+  const utorid = 'harsima1'; // Replace with your actual UTorID
+    const htmlResponse = `
+        <div id="auto-tester">
+              Hi. I am ${utorid}, and I have learned how to develop a full-stack web app
+                    and deploy it!
+                        </div>
+                          `;
+
+                              return new Response(htmlResponse, {
+                                  headers: {
+                                        'Content-Type': 'text/html',
+                                            },
+                                              });
+                                              }
